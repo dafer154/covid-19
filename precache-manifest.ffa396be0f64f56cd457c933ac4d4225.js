0da1ebe98bbedd93133c672b5e9ed99968b97d0f
Camilo Alzate <caalzate91@gmail.com>
2020-07-15 18:04:30 -0500

    Updates

---
+++ precache-manifest.ffa396be0f64f56cd457c933ac4d4225.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae775ab8c1ca9db7bf5c4ad709df69b4",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "2222edddf905421b1465",
+    "url": "/covid-19/static/css/main.e734da63.chunk.css"
+  },
+  {
+    "revision": "42a2e7783875b7eb5519",
+    "url": "/covid-19/static/js/2.f8fa2697.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.f8fa2697.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2222edddf905421b1465",
+    "url": "/covid-19/static/js/main.9a24b3db.chunk.js"
+  },
+  {
+    "revision": "ca6f7991abbe432f93f7",
+    "url": "/covid-19/static/js/runtime-main.8e18646f.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
